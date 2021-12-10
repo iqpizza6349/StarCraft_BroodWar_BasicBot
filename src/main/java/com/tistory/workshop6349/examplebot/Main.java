c684770a938d0a27f7698d30df23605f1a79c9cb
iqpizza4678 <iqpizza4678@gmail.com>
2021-12-10 23:36:31 +0900

    read: comment line

--- src/main/java/com/tistory/workshop6349/examplebot/Main.java
+++ src/main/java/com/tistory/workshop6349/examplebot/Main.java
@@ -2,6 +2,12 @@ package com.tistory.workshop6349.examplebot;
 
 public class Main {
 
+    /**
+     * has problem in attacking
+     * @see ExampleBot base bugs
+     * @see UnitInfo bugs
+     */
+
     public static void main(String[] args) {
         try {
             new ExampleBot().run();
