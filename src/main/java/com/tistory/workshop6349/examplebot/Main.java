53ea22362482d6167f53a0fb87efeb25f455101b
iqpizza6349 <iqpizza6349@gmail.com>
2021-12-11 11:42:21 +0900

    update: clean codes

--- src/main/java/com/tistory/workshop6349/examplebot/Main.java
+++ src/main/java/com/tistory/workshop6349/examplebot/Main.java
@@ -2,6 +2,8 @@ package com.tistory.workshop6349.examplebot;
 
 public class Main {
 
+    // 현재 인텔리제이 깃허브가 오류가 나서 고치는 중입니다.
+
     public static void main(String[] args) {
         try {
             new ExampleBot().run();
